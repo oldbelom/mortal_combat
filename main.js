acc961525165ca7c6043a190ba733b61ec9d76fa
oldbelom <oldbelom@gmail.com>
2021-04-15 20:53:07 +0300

    added attack and defense functionality

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const $arenas = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button');
+const $fightButton = document.querySelector('.button');
+const $formFight = document.querySelector('.control');
+
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20
+}
+
+const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
     player: 1,
@@ -7,12 +16,10 @@ const player1 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['sword'],
-    attack: function(name) {
-        console.log(name + ' Fight...');
-    },
-    changeHP: changeHP,
-    elHP: elHP,
-    renderHP: renderHP
+    attack,
+    changeHP,
+    elHP,
+    renderHP
 }
 
 const player2 = {
@@ -21,12 +28,14 @@ const player2 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['fan'],
-    attack: function(name) {
-        console.log(name + ' Fight...');
-    },
-    changeHP: changeHP,
-    elHP: elHP,
-    renderHP: renderHP
+    attack,
+    changeHP,
+    elHP,
+    renderHP
+}
+
+function attack() {
+    console.log(this.name + ' Fight...');
 }
 
 function createElement(tag, className) {
@@ -105,17 +114,61 @@ function createReloadButton() {
     })
 }
 
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1];
+    const defence = ATTACK[getRandom(3) - 1];
+    
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence
+    }
+}
+
+function playerAttack() {
+    const attack = {};
+
+    for (let item of $formFight) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value;
+        }
+
+        if (item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+
+        item.checked = false;
+    }
+
+    return attack;
+}
+
+function checkDamage() {
+    const enemy = enemyAttack();
+    const attack = playerAttack();
+
+    if (attack.hit !== enemy.defence) {
+        player2.changeHP(attack.value);
+        player2.renderHP();
+    }
+
+    if (attack.defence !== enemy.hit) {
+        player1.changeHP(enemy.value);
+        player1.renderHP();
+    }
+}
+
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
 
-$randomButton.addEventListener('click', function() {
-    player1.changeHP(getRandom(20));
-    player2.changeHP(getRandom(20));
-    player1.renderHP();
-    player2.renderHP();
+$formFight.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    checkDamage();
 
     if (player1.hp === 0 || player2.hp === 0) {
-        $randomButton.disabled = true;
+        $formFight.classList.add('hide');
         createReloadButton();
     }
 
@@ -127,6 +180,3 @@ $randomButton.addEventListener('click', function() {
         $arenas.appendChild(playerWins());
     }
 })
-
-player1.attack('Subzero');
-player2.attack('Kitana');
